2bf9bb9c31e92dff278c28d75f2d3ce9cbda77e6
Edilson Silva <eddysilva1994@gmail.com>
2026-03-07 00:47:21 -0300

    feat: add login page

--- src/view/pages/Login/index.tsx
+++ src/view/pages/Login/index.tsx
@@ -1,3 +1,34 @@
+import { Link } from "react-router-dom";
+import { AppRoutes } from "../../../Router/constants";
+import { Button } from "../../components/Button";
+import { Input } from "../../components/Input";
+
 export function Login() {
-  return <h1>Login</h1>;
+  return (
+    <>
+      <header className="flex flex-col items-center gap-4 text-center">
+        <h1 className="text-2xl font-bold text-gray-900 tracking-[-1px]">
+          Entre em sua conta
+        </h1>
+        <p className="space-x-2">
+          <span className="text-gray-700 text-base font-normal tracking-[-0.5px]">
+            Novo por aqui?
+          </span>
+          <Link
+            to={AppRoutes.REGISTER}
+            className="text-teal-900 text-base font-medium tracking-[-0.5px]"
+          >
+            Crie uma conta
+          </Link>
+        </p>
+      </header>
+      <form className="mt-[60px] flex flex-col gap-4">
+        <Input type="email" name="email" placeholder="E-mail" />
+        <Input type="password" name="password" placeholder="Senha" />
+        <Button type="submit" className="mt-2">
+          Entrar
+        </Button>
+      </form>
+    </>
+  );
 }
