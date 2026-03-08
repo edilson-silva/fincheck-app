a45b226ff1f2b1685021bab01e0c75842d5c33f0
Edilson Silva <eddysilva1994@gmail.com>
2026-03-08 02:32:25 -0300

    feat: using uncontrolled components to handle login form state using react hook form and zod

--- src/view/pages/Login/index.tsx
+++ src/view/pages/Login/index.tsx
@@ -1,24 +1,38 @@
+import { Link } from "react-router-dom";
 import { AppRoutes } from "../../../Router/constants";
-import { AuthForm } from "../../components/AuthForm";
 import { Button } from "../../components/Button";
 import { Input } from "../../components/Input";
+import { useLoginController } from "./useLoginController";
 
 export function Login() {
+  const { handleSubmit, register, errors } = useLoginController();
   return (
-    <AuthForm
-      title="Entre em sua conta"
-      subtitle="Novo por aqui?"
-      linkTitle="Crie uma conta"
-      linkTo={AppRoutes.REGISTER}
-      children={
-        <>
-          <Input type="email" name="email" placeholder="E-mail" />
-          <Input type="password" name="password" placeholder="Senha" />
-          <Button type="submit" className="mt-2">
-            Entrar
-          </Button>
-        </>
-      }
-    />
+    <>
+      <header className="flex flex-col items-center gap-4 text-center">
+        <h1 className="text-2xl font-bold text-gray-900 tracking-[-1px]">
+          Entre em sua conta
+        </h1>
+        <p className="space-x-2">
+          <span className="text-gray-700 text-base font-normal tracking-[-0.5px]">
+            Novo por aqui?
+          </span>
+          <Link
+            to={AppRoutes.REGISTER}
+            className="text-teal-900 text-base font-medium tracking-[-0.5px]"
+          >
+            Crie uma conta
+          </Link>
+        </p>
+      </header>
+      <form className="mt-[60px] flex flex-col gap-4" onSubmit={handleSubmit}>
+        <Input type="email" placeholder="E-mail" {...register("email")} />
+        {errors.email && <span>{errors.email.message}</span>}
+        <Input type="password" placeholder="Senha" {...register("password")} />
+        {errors.password && <span>{errors.password.message}</span>}
+        <Button type="submit" className="mt-2">
+          Entrar
+        </Button>
+      </form>
+    </>
   );
 }
---
+++ src/view/pages/Login/useLoginController.tsx
@@ -0,0 +1,29 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import z from "zod";
+
+const schema = z.object({
+  email: z.email("Informe um e-mail válido").nonempty("E-mail é obrigatório"),
+  password: z
+    .string()
+    .nonempty("Senha é obrigatória")
+    .min(8, "Senha deve conter pelo menos 8 dígitos"),
+});
+
+type FormData = z.infer<typeof schema>;
+
+export function useLoginController() {
+  const {
+    handleSubmit: hookFormHandleSubmit,
+    register,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+  });
+
+  const handleSubmit = hookFormHandleSubmit((data) => {
+    console.log("CHAMA API:", data);
+  });
+
+  return { handleSubmit, register, errors };
+}
