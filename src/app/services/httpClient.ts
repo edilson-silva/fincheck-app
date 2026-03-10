b9ffd0eb7a5a0435aa082f508a7b5a23fd6d4b7c
Edilson Silva <eddysilva1994@gmail.com>
2026-03-09 23:30:59 -0300

    feat: setup http client intereceptor to add authorization header if token is available

--- src/app/services/httpClient.ts
+++ src/app/services/httpClient.ts
@@ -1,5 +1,17 @@
 import axios from "axios";
+import { Constants } from "../config/constants";
+import { StorageUtil } from "../utils/storage";
 
 export const httpClient = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
+});
+
+httpClient.interceptors.request.use((config) => {
+  const accessToken = StorageUtil.getItem(Constants.ACCESS_TOKEN);
+
+  if (accessToken) {
+    config.headers.Authorization = `Bearer ${accessToken}`;
+  }
+
+  return config;
 });
