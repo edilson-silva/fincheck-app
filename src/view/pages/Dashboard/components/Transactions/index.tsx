03ef168c9b7734604fe3b187946b844d8955c6f1
Edilson Silva <eddysilva1994@gmail.com>
2026-03-13 23:43:37 -0300

    feat: add mock state and style for empty transactions list

--- src/view/pages/Dashboard/components/Transactions/index.tsx
+++ src/view/pages/Dashboard/components/Transactions/index.tsx
@@ -6,23 +6,27 @@ import { formatCurrencty } from "../../../../../app/utils/currency";
 import { CategoryIcon } from "../../../../../assets/icons/categories/CategoryIcon";
 import { FilterIcon } from "../../../../../assets/icons/FilterIcon";
 import { TransactionsIcon } from "../../../../../assets/icons/TransactionsIcon";
+import emptyStateImage from "../../../../../assets/images/empty-state.svg";
 import { Spinner } from "../../../../components/Spinner";
 import { SliderNavigation } from "./SliderNavigation";
 import { SliderOption } from "./SliderOption";
 import { useTransactionsController } from "./useTransactionsController";
 
 export function Transactions() {
-  const { areValuesVisible, isLoading } = useTransactionsController();
+  const { areValuesVisible, isInitialLoading, isLoading, transactions } =
+    useTransactionsController();
+
+  const hasTransactions = transactions.length > 0;
 
   return (
     <div className="bg-gray-200 rounded-2xl w-full h-full p-10 flex flex-col">
-      {isLoading && (
+      {isInitialLoading && (
         <div className="w-full h-full flex items-center justify-center">
           <Spinner className="w-10 h-10" />
         </div>
       )}
 
-      {!isLoading && (
+      {!isInitialLoading && (
         <>
           <header className="">
             <div className="flex items-center justify-between">
@@ -55,44 +59,63 @@ export function Transactions() {
             </div>
           </header>
           <div className="mt-4 space-y-2 flex-1 overflow-y-auto">
-            <div className="bg-white p-4 rounded-2xl flex items-center jusitfy-between gap-4">
-              <div className="flex-1 flex items-center gap-3">
-                <CategoryIcon type="expense" />
-                <div>
-                  <strong className="font-bold tracking-[-0.5px] block">
-                    Almoço
-                  </strong>
-                  <span className="text-sm text-gray-600">04/06/2026</span>
-                </div>
+            {isLoading && (
+              <div className="flex flex-col items-center justify-center h-full">
+                <Spinner className="w-10 h-10" />
               </div>
-              <span
-                className={cn(
-                  "text-red-800 tracking-[-0.5px] font-medium",
-                  !areValuesVisible && "blur-sm",
-                )}
-              >
-                {formatCurrencty(-123)}
-              </span>
-            </div>
-            <div className="bg-white p-4 rounded-2xl flex items-center jusitfy-between gap-4">
-              <div className="flex-1 flex items-center gap-3">
-                <CategoryIcon type="income" />
-                <div>
-                  <strong className="font-bold tracking-[-0.5px] block">
-                    Salário
-                  </strong>
-                  <span className="text-sm text-gray-600">04/06/2026</span>
-                </div>
+            )}
+
+            {!hasTransactions && !isLoading && (
+              <div className="flex flex-col items-center justify-center h-full">
+                <img src={emptyStateImage} alt="Empty State" />
+                <span className="text-gray-700">
+                  Não encontramos nenhuma transação
+                </span>
               </div>
-              <span
-                className={cn(
-                  "text-green-800 tracking-[-0.5px] font-medium",
-                  !areValuesVisible && "blur-sm",
-                )}
-              >
-                {formatCurrencty(5000)}
-              </span>
-            </div>
+            )}
+
+            {hasTransactions && !isLoading && (
+              <>
+                <div className="bg-white p-4 rounded-2xl flex items-center jusitfy-between gap-4">
+                  <div className="flex-1 flex items-center gap-3">
+                    <CategoryIcon type="expense" />
+                    <div>
+                      <strong className="font-bold tracking-[-0.5px] block">
+                        Almoço
+                      </strong>
+                      <span className="text-sm text-gray-600">04/06/2026</span>
+                    </div>
+                  </div>
+                  <span
+                    className={cn(
+                      "text-red-800 tracking-[-0.5px] font-medium",
+                      !areValuesVisible && "blur-sm",
+                    )}
+                  >
+                    {formatCurrencty(-123)}
+                  </span>
+                </div>
+                <div className="bg-white p-4 rounded-2xl flex items-center jusitfy-between gap-4">
+                  <div className="flex-1 flex items-center gap-3">
+                    <CategoryIcon type="income" />
+                    <div>
+                      <strong className="font-bold tracking-[-0.5px] block">
+                        Salário
+                      </strong>
+                      <span className="text-sm text-gray-600">04/06/2026</span>
+                    </div>
+                  </div>
+                  <span
+                    className={cn(
+                      "text-green-800 tracking-[-0.5px] font-medium",
+                      !areValuesVisible && "blur-sm",
+                    )}
+                  >
+                    {formatCurrencty(5000)}
+                  </span>
+                </div>
+              </>
+            )}
           </div>
         </>
       )}
