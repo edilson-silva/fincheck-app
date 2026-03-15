a8a56df13684292c70cf166453ce0fa0a73a28f3
Edilson Silva <eddysilva1994@gmail.com>
2026-03-15 01:53:38 -0300

    fix: set dropdown menu content z-index to diplay it in the view top

--- src/view/components/DropdownMenu/DropdownMenuContent.tsx
+++ src/view/components/DropdownMenu/DropdownMenuContent.tsx
@@ -15,7 +15,7 @@ export function DropdownMenuContent({
     <RadixDropdownMenu.Portal>
       <RadixDropdownMenu.Content
         className={cn(
-          "rounded-2xl p-2 bg-white space-y-2 box-shadow-[0px_11px_20px_0px_rgba(0,0,0,0.10)]",
+          "rounded-2xl p-2 bg-white space-y-2 box-shadow-[0px_11px_20px_0px_rgba(0,0,0,0.10)] z-50",
           className,
         )}
       >
