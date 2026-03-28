ffa96e961a98c3d14009d6cc2704cd5091ea941b
Edilson Silva <eddysilva1994@gmail.com>
2026-03-28 00:29:03 -0300

    feat: add floating action button

--- src/view/components/DropdownMenu/DropdownMenuContent.tsx
+++ src/view/components/DropdownMenu/DropdownMenuContent.tsx
@@ -16,6 +16,8 @@ export function DropdownMenuContent({
       <RadixDropdownMenu.Content
         className={cn(
           "rounded-2xl p-2 bg-white space-y-2 box-shadow-[0px_11px_20px_0px_rgba(0,0,0,0.10)] z-50",
+          "data-[side=bottom]:animate-slide-up-and-fade",
+          "data-[side=top]:animate-slide-down-and-fade",
           className,
         )}
       >
