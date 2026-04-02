53ddc64595dda1fa094c3ea286f55ef77e534250
Edilson Silva <eddysilva1994@gmail.com>
2026-04-02 00:33:20 -0300

    feat: show input currency error and add change value event param

--- src/view/components/InputCurrency.tsx
+++ src/view/components/InputCurrency.tsx
@@ -1,12 +1,26 @@
 import { NumericFormat } from "react-number-format";
+import { cn } from "../../app/utils/cn";
+import { ErrorInfo } from "./ErrorInfo";
 
-export function InputCurrency() {
+interface InputCurrencyProps {
+  error?: string;
+  onChange: (value: string) => void;
+}
+
+export function InputCurrency({ error, onChange }: InputCurrencyProps) {
   return (
-    <NumericFormat
-      className="w-full text-gray-800 text-[32px] font-bold tracking-[-1px] outline-none"
-      thousandSeparator="."
-      decimalSeparator=","
-      defaultValue="0"
-    />
+    <div>
+      <NumericFormat
+        className={cn(
+          "w-full text-gray-800 text-[32px] font-bold tracking-[-1px] outline-none",
+          error && "text-red-900",
+        )}
+        thousandSeparator="."
+        decimalSeparator=","
+        defaultValue="0"
+        onValueChange={(values) => onChange(values.value)}
+      />
+      {error && <ErrorInfo error={error} />}
+    </div>
   );
 }
